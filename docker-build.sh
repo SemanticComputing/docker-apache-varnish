4556a11246e39d77c183a4173095571e6f6c0a58
Miika Piiroinen <miika.piiroinen@aalto.fi>
2018-02-15 12:55:56 +0200

    Add --no-cache as an optional in the docker run script with ./docker-run.sh -c

--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,13 @@
 #! /bin/bash
 
-docker build --no-cache -t apache-varnish-php5 .
+PARAMS=""
+
+while getopts ":c" opt; do
+    case ${opt} in
+        c)
+            PARAMS="$PARAMS --no-cache"
+            ;;
+    esac
+done
+
+docker build $PARAMS -t apache-varnish-php5 .
